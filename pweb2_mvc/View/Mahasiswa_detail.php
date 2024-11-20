66761f70ec20f424b645935487a30a339f784da8
AdityaNugroho07 <168439946+AdityaNugroho07@users.noreply.github.com>
2024-11-20 15:16:14 +0700

    Update Mahasiswa_detail.php

--- pweb2_mvc/View/Mahasiswa_detail.php
+++ pweb2_mvc/View/Mahasiswa_detail.php
@@ -6,17 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-<h2>Tambah Pengguna Baru</h2>
-<form action="/user/store" method="POST">
-    <label for="name">Nama:</label>
-    <input type="text" name="name" id="name" required>
-    <label for="nim">NIM:</label>
-    <input type="nim" name="nim" id="nim" required> 
-    <label for="prodi">Prodi:</label>
-    <input type="prodi" name="prodi" id="prodi" required>
-    <button type="submit">Simpan</button>
-</form>
-
     <table>
         <tr>
             <th>Nama</th>
